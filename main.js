eaf25a4909811456a091ac23a64594e318620048
Vane <88143635+evan052006@users.noreply.github.com>
2024-10-27 16:06:14 +0700

    Polishing game logic
    Can actually play Tic Tac Toe now
    sorry too lazy to write more detailed msg

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ class Game {
         this.height = height;
         this.width = width;
         this.consecutiveMarksToWin = consecutiveMarksToWin;
-        this.player1 = 'X';
-        this.player2 = 'O';
+        this.players = ['X', 'O'];
+        this.playerCount = 2;
+        this.round = 0;
         this.buildBoard();
+        this.currentTurnBar = document.querySelector('#currentPlayer');
+        this.currentTurnBar.innerHTML = this.players[0];
+        this.gameEnd = false;
     }
-
+    
     buildBoard(){
         const board = document.createElement('div');
         board.classList.add('board');
@@ -28,6 +32,8 @@ class Game {
             grid[i] = [];
             for (let j = 0; j < this.width; j++){
                 const cell = document.createElement('div');
+                cell.dataset.x = j;
+                cell.dataset.y = i;
                 cell.classList.add('cell');
                 cellObserver.observe(cell);
                 board.appendChild(cell);
@@ -35,18 +41,39 @@ class Game {
             }   
         }
 
-
+        board.addEventListener('click', (event) => this.cellClicked(event));
         document.body.appendChild(board);
         this.boardElement = board;
         this.grid = grid;
     }
 
+    cellClicked(event){
+        const cell = event.target;
+        if (!this.gameEnd && cell.classList.contains('cell') && !this.isEmptyOrSameSymbol(cell)){ // means empty
+            this.advanceGame(cell);
+        }
+    }
+
+    advanceGame(updatedCell){
+        const lastPosition = {x: parseInt(updatedCell.dataset.x, 10), y: parseInt(updatedCell.dataset.y, 10)};
+        console.log(lastPosition);
+        updatedCell.innerHTML = this.currentTurnBar.innerHTML;
+        const enoughRounds = this.round >= this.playerCount * (this.consecutiveMarksToWin - 1);
+        if (enoughRounds && this.checkWin(this.currentTurnBar.innerHTML, lastPosition)){
+            alert('WIN :>');
+            this.gameEnd = true;
+        }
+        this.round++;
+        this.currentTurnBar.innerHTML = this.players[this.round % this.playerCount];
+
+    }
 
     checkWin(lastPlayerSymbol, lastPosition){
+
         const shiftHorizontal = (position, distance) => ({x: position.x + distance, y: position.y});
         const shiftVertical = (position, distance) => ({x: position.x, y: position.y + distance});
         const shiftDiagonalUp = (position, distance) => ({x: position.x + distance, y: position.y - distance});
-        const shiftDiagonalDown = (position, distance) => ({x: position.x - distance, y: position.y + distance});
+        const shiftDiagonalDown = (position, distance) => ({x: position.x + distance, y: position.y + distance});
         const checkHorizontal = this.checkConsecutiveMarks(lastPlayerSymbol, lastPosition, shiftHorizontal);
         const checkVertical = this.checkConsecutiveMarks(lastPlayerSymbol, lastPosition, shiftVertical);
         const checkDiagonalUp = this.checkConsecutiveMarks(lastPlayerSymbol, lastPosition, shiftDiagonalUp);
@@ -77,11 +104,15 @@ class Game {
     }
 
     inBounds(position){
-        return position.x >= this.width || position.x < 0 || position.y >= this.height || position.y < 0;
+        return !(position.x >= this.width || position.x < 0 || position.y >= this.height || position.y < 0);
     }
 
     getCell(position){
         return this.grid[position.y][position.x].innerHTML;
+    }
+
+    isEmptyOrSameSymbol(cell){
+        return cell.innerHTML || cell.innerHTML == (this.players[this.round % this.playerCount]);
     }
 
     updateCell(position, symbol){
@@ -89,7 +120,5 @@ class Game {
     }
 }
  
-const game = new Game(25, 25, 3);
-for (let i = 0; i < game.width; i++){
-    game.updateCell({x: i, y: i}, 'O');
-}
+const game = new Game(11, 11, 5);
+
